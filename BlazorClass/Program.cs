f17d8822601e4af0d8c457291ef127ebb5e9a391
Joseph Whitmire <113870560+TheToppestHat@users.noreply.github.com>
2023-11-29 13:37:16 -0500

    Please work

--- BlazorClass/Program.cs
+++ BlazorClass/Program.cs
@@ -14,6 +14,7 @@ namespace BlazorClass
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddSingleton<WeatherForecastService>();
+            
 
             var app = builder.Build();
 
